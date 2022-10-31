00c33cd3e19a794ae45c62b4e4e1d532cd40e93a
Zaid Almuflahi <zaid.almuflahi1217@gmail.com>
2022-10-30 21:07:48 -0400

    commit tree c63a8d4d2a0ad517f3c9bed788f07a4f6ad898bb

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Add your code here
+function submitData(userName, userEmail){
+    const ul = document.querySelector('ul')
+    const li = document.createElement('li')
+    const data = {
+        name: userName, 
+        email: userEmail
+}
+const config = {
+    method: 'POST',
+    headers: {
+        'content-type': 'application/json',
+        accept: 'application/json'
+    },
+    body: JSON.stringify(data),
+}
+return fetch('http://localhost:3000/users', config)
+.then(function (response) {
+    return response.json();
+  })
+  .then(function (res) {
+    li.innerHTML = 'Id' + res.id
+    ul.appendChild(li)
+  })
+  .catch(function (error) {
+   let h3 = document.createElement('h3')
+   h3.innerHTML = error.message
+   document.body.appendChild(h3)
+  });
+}
+submitData('zaid', 'almuflahi')
